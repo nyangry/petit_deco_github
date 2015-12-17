693762e1999953e5acc72c347433b9afc0db4aeb
Daisuke Masuyama <masuyama.daisuke@moneyforward.co.jp>
2015-12-17 18:59:58 +0900

    Implement EmojiPallet

--- src/js/main.coffee
+++ src/js/main.coffee
@@ -48,16 +48,106 @@ $ ->
 
 
   class EmojiPallet
+    selectors:
+      starter: '.js-petit-deco-insert-emoji-pallet'
+      backdrop: '.js-petit-deco-emoji-pallet-backdrop'
+      pallet: '.emoji-suggestions'
+
     constructor: ->
-      @insertIcon()
-      @bindEvents()
+      @deferred = new $.Deferred
 
-    insertIcon: ->
+      @insertEmojiPalletBackdrop()
+      @deferred.promise().then(@fetchSuggestions)
+      @deferred.promise().then(@insertIcon)
+      @deferred.promise().then(@bindEvents)
 
-    bindEvents: ->
+    insertEmojiPalletBackdrop: =>
+      $emoji_pallet_backdrop_node = $('<div/>').attr
+        class: @selectors['backdrop']
+        style: 'z-index: 100; display:none; position:fixed; top:0; left:0; width:100%; height:120%;'
+
+      $emoji_pallet_backdrop_node.appendTo 'body'
+
+      @deferred.resolve()
+
+    fetchSuggestions: =>
+      $.ajax($('.js-suggester').first().data('url')).done (suggestions) =>
+        $(suggestions).appendTo(@selectors['backdrop'])
+
+        $(@selectors['pallet']).find('li').each ->
+          $span = $(@).find('span')
+
+          $(@).html($span)
+
+          $(@).css(
+            display: 'inline-block'
+          )
+
+        @deferred.resolve()
+
+    insertIcon: =>
+      $icon_node = $('<span/>').attr(
+        class: 'octicon octicon-octoface'
+      )
+
+      $new_tab = $('<div/>').attr(
+        class: 'tabnav-tab ' + @selectors['starter'].replace(/\./, '')
+        style: 'cursor: pointer;'
+      ).append($icon_node)
+
+      $(COMMON_SELECTORS.PREVIEWABLE_COMMENT_FORM).each ->
+        $(@).find('.tabnav-tabs').append $new_tab.clone()
+
+      @deferred.resolve()
+
+    bindEvents: =>
+      $('body').on 'click', '.' + @selectors['starter'], (e) ->
+        $self = $(@)
+
+        $emoji_pallet_backdrop_node = $(@selectors['backdrop'])
+        $emoji_pallet_backdrop_node.show()
+
+        $emoji_pallet_node = $(@selectors['pallet'])
+        $emoji_pallet_node.show()
+        $emoji_pallet_node.css 'top', $self.offset().top + 42
+        # $emoji_pallet_node.css 'left', $self.offset().left + 18
+        $emoji_pallet_node.css 'left', '25%'
+
+        # 現在開いているコメント欄を保存しておく
+        $current_form = $(@).parents('form')
+
+      # 絵文字パレット以外の領域クリックでパレットを閉じる
+      $('body').on 'click', @selectors['backdrop'], (e) ->
+        $(@).hide()
+        $emoji_pallet_node = $('.js-pallet')
+        $emoji_pallet_node.hide()
+
+      # 絵文字を選択する
+      $('body').on 'click', '.js-pallet-icon', (e) ->
+        $text_area = $current_form.find('.js-note-text')
+
+        # 絵文字タグを挿入する
+        $text_area.val $text_area.val() + ' ' + $(@).data 'emoji'
+
+        # コメント追加ボタンのdisabledを解除する
+        $current_form.find('.js-comment-button').removeClass('disabled').removeAttr('disabled')
+
+        # パレットを閉じる
+        $emoji_pallet_backdrop_node = $('.js-pallet-backdrop')
+        $emoji_pallet_backdrop_node.hide()
+
+        $emoji_pallet_node = $('.js-pallet')
+        $emoji_pallet_node.hide()
+
+        # テキストエリアにフォーカスする
+        $text_area.focus()
+
+      @deferred.resolve()
 
 
   decoratePreviewableCommentForm = ->
+    return if $(COMMON_SELECTORS.PREVIEWABLE_COMMENT_FORM).length is 0
+
     new PlusOne
     new LGTMImageSelection
     new EmojiPallet
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+var bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
 $(function() {
   var COMMON_SELECTORS, EmojiPallet, LGTMImageSelection, PlusOne, decoratePreviewableCommentForm;
   COMMON_SELECTORS = {
@@ -55,19 +57,105 @@ $(function() {
 
   })();
   EmojiPallet = (function() {
+    EmojiPallet.prototype.selectors = {
+      starter: '.js-petit-deco-insert-emoji-pallet',
+      backdrop: '.js-petit-deco-emoji-pallet-backdrop',
+      pallet: '.emoji-suggestions'
+    };
+
     function EmojiPallet() {
-      this.insertIcon();
-      this.bindEvents();
+      this.bindEvents = bind(this.bindEvents, this);
+      this.insertIcon = bind(this.insertIcon, this);
+      this.fetchSuggestions = bind(this.fetchSuggestions, this);
+      this.insertEmojiPalletBackdrop = bind(this.insertEmojiPalletBackdrop, this);
+      this.deferred = new $.Deferred;
+      this.insertEmojiPalletBackdrop();
+      this.deferred.promise().then(this.fetchSuggestions);
+      this.deferred.promise().then(this.insertIcon);
+      this.deferred.promise().then(this.bindEvents);
     }
 
-    EmojiPallet.prototype.insertIcon = function() {};
+    EmojiPallet.prototype.insertEmojiPalletBackdrop = function() {
+      var $emoji_pallet_backdrop_node;
+      $emoji_pallet_backdrop_node = $('<div/>').attr({
+        "class": this.selectors['backdrop'],
+        style: 'z-index: 100; display:none; position:fixed; top:0; left:0; width:100%; height:120%;'
+      });
+      $emoji_pallet_backdrop_node.appendTo('body');
+      return this.deferred.resolve();
+    };
 
-    EmojiPallet.prototype.bindEvents = function() {};
+    EmojiPallet.prototype.fetchSuggestions = function() {
+      return $.ajax($('.js-suggester').first().data('url')).done((function(_this) {
+        return function(suggestions) {
+          $(suggestions).appendTo(_this.selectors['backdrop']);
+          $(_this.selectors['pallet']).find('li').each(function() {
+            var $span;
+            $span = $(this).find('span');
+            $(this).html($span);
+            return $(this).css({
+              display: 'inline-block'
+            });
+          });
+          return _this.deferred.resolve();
+        };
+      })(this));
+    };
+
+    EmojiPallet.prototype.insertIcon = function() {
+      var $icon_node, $new_tab;
+      $icon_node = $('<span/>').attr({
+        "class": 'octicon octicon-octoface'
+      });
+      $new_tab = $('<div/>').attr({
+        "class": 'tabnav-tab ' + this.selectors['starter'].replace(/\./, ''),
+        style: 'cursor: pointer;'
+      }).append($icon_node);
+      $(COMMON_SELECTORS.PREVIEWABLE_COMMENT_FORM).each(function() {
+        return $(this).find('.tabnav-tabs').append($new_tab.clone());
+      });
+      return this.deferred.resolve();
+    };
+
+    EmojiPallet.prototype.bindEvents = function() {
+      $('body').on('click', '.' + this.selectors['starter'], function(e) {
+        var $current_form, $emoji_pallet_backdrop_node, $emoji_pallet_node, $self;
+        $self = $(this);
+        $emoji_pallet_backdrop_node = $(this.selectors['backdrop']);
+        $emoji_pallet_backdrop_node.show();
+        $emoji_pallet_node = $(this.selectors['pallet']);
+        $emoji_pallet_node.show();
+        $emoji_pallet_node.css('top', $self.offset().top + 42);
+        $emoji_pallet_node.css('left', '25%');
+        return $current_form = $(this).parents('form');
+      });
+      $('body').on('click', this.selectors['backdrop'], function(e) {
+        var $emoji_pallet_node;
+        $(this).hide();
+        $emoji_pallet_node = $('.js-pallet');
+        return $emoji_pallet_node.hide();
+      });
+      $('body').on('click', '.js-pallet-icon', function(e) {
+        var $emoji_pallet_backdrop_node, $emoji_pallet_node, $text_area;
+        $text_area = $current_form.find('.js-note-text');
+        $text_area.val($text_area.val() + ' ' + $(this).data('emoji'));
+        $current_form.find('.js-comment-button').removeClass('disabled').removeAttr('disabled');
+        $emoji_pallet_backdrop_node = $('.js-pallet-backdrop');
+        $emoji_pallet_backdrop_node.hide();
+        $emoji_pallet_node = $('.js-pallet');
+        $emoji_pallet_node.hide();
+        return $text_area.focus();
+      });
+      return this.deferred.resolve();
+    };
 
     return EmojiPallet;
 
   })();
   decoratePreviewableCommentForm = function() {
+    if ($(COMMON_SELECTORS.PREVIEWABLE_COMMENT_FORM).length === 0) {
+      return;
+    }
     new PlusOne;
     new LGTMImageSelection;
     return new EmojiPallet;
