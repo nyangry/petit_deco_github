b7f329664cff789113427118d8dbe837c8429815
nyangry <iacta.alea.esto@gmail.com>
2018-04-19 17:40:08 +0900

    Implement eventPage.js

--- dist/eventPage.js
+++ dist/eventPage.js
@@ -1,44 +1,45 @@
 chrome.runtime.onConnect.addListener(function(port) {
-  port.onMessage.addListener(function() {
-    var get_json_request = new XMLHttpRequest();
+  port.onMessage.addListener(function(lgtm_image_index) {
+    let get_json_request = new XMLHttpRequest()
 
-    get_json_request.open('GET', 'https://lgtm.in/g', true);
-    get_json_request.setRequestHeader('Accept', 'application/json');
+    get_json_request.open('GET', 'https://lgtm.in/g', true)
+    get_json_request.setRequestHeader('Accept', 'application/json')
 
     get_json_request.onload = function() {
       if (get_json_request.status >= 200 && get_json_request.status < 400) {
-        var data             = JSON.parse(get_json_request.responseText);
-        var image_source_url = data.actualImageUrl;
-        var markdown         = `![](${image_source_url})`;
-        var image            = new Image();
-        var base64_image     = null;
+        let data             = JSON.parse(get_json_request.responseText)
+        let image_source_url = data.actualImageUrl
+        let markdown         = `![](${image_source_url})`
+        let image            = new Image()
+        let base64_image     = null
 
         image.onload = function() {
-          var canvas    = document.createElement('canvas');
-          canvas.width  = this.width;
-          canvas.height = this.height;
+          let canvas    = document.createElement('canvas')
+          canvas.width  = this.width
+          canvas.height = this.height
 
-          var ctx = canvas.getContext('2d');
-          ctx.drawImage(this, 0, 0);
+          let ctx = canvas.getContext('2d')
+          ctx.drawImage(this, 0, 0)
 
-          base64_image = canvas.toDataURL('image/jpeg');
+          base64_image = canvas.toDataURL('image/jpeg')
 
-          canvas = null;
+          canvas = null
 
           port.postMessage({
+            lgtm_image_index: lgtm_image_index,
             markdown: markdown,
             base64_image: base64_image
-          });
-        };
+          })
+        }
 
-        image.src = image_source_url;
+        image.src = image_source_url
       }
-    };
+    }
 
     get_json_request.onerror = function() {
       // There was a connection error of some sort
-    };
+    }
 
-    get_json_request.send();
-  });
-});
+    get_json_request.send()
+  })
+})
