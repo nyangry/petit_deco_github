efc3af17e9cf8215cb19751c7a022395f8e70333
nyangry <iacta.alea.esto@gmail.com>
2019-08-28 16:26:14 +0900

    Format dist/eventPage.js

--- dist/eventPage.js
+++ dist/eventPage.js
@@ -1,45 +1,45 @@
-chrome.runtime.onConnect.addListener(function(port) {
-  port.onMessage.addListener(function(lgtm_image_index) {
-    let get_json_request = new XMLHttpRequest()
+chrome.runtime.onConnect.addListener((port) => {
+  port.onMessage.addListener((lgtm_image_index) => {
+    const get_json_request = new XMLHttpRequest();
 
-    get_json_request.open('GET', 'https://lgtm.in/g', true)
-    get_json_request.setRequestHeader('Accept', 'application/json')
+    get_json_request.open('GET', 'https://lgtm.in/g', true);
+    get_json_request.setRequestHeader('Accept', 'application/json');
 
-    get_json_request.onload = function() {
+    get_json_request.onload = function () {
       if (get_json_request.status >= 200 && get_json_request.status < 400) {
-        let data             = JSON.parse(get_json_request.responseText)
-        let image_source_url = data.actualImageUrl
-        let markdown         = `![](${image_source_url})`
-        let image            = new Image()
-        let base64_image     = null
+        const data             = JSON.parse(get_json_request.responseText);
+        const image_source_url = data.actualImageUrl;
+        const markdown         = `![](${image_source_url})`;
+        const image            = new Image();
+        let base64_image     = null;
 
-        image.onload = function() {
-          let canvas    = document.createElement('canvas')
-          canvas.width  = this.width
-          canvas.height = this.height
+        image.onload = function () {
+          let canvas    = document.createElement('canvas');
+          canvas.width = this.width;
+          canvas.height = this.height;
 
-          let ctx = canvas.getContext('2d')
-          ctx.drawImage(this, 0, 0)
+          const ctx = canvas.getContext('2d');
+          ctx.drawImage(this, 0, 0);
 
-          base64_image = canvas.toDataURL('image/jpeg')
+          base64_image = canvas.toDataURL('image/jpeg');
 
-          canvas = null
+          canvas = null;
 
           port.postMessage({
-            lgtm_image_index: lgtm_image_index,
-            markdown: markdown,
-            base64_image: base64_image
-          })
-        }
+            lgtm_image_index,
+            markdown,
+            base64_image,
+          });
+        };
 
-        image.src = image_source_url
+        image.src = image_source_url;
       }
-    }
+    };
 
-    get_json_request.onerror = function() {
+    get_json_request.onerror = function () {
       // There was a connection error of some sort
-    }
+    };
 
-    get_json_request.send()
-  })
-})
+    get_json_request.send();
+  });
+});
